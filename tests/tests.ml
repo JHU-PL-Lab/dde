eb94b4f520f42d4c9607214f420951841ddf62a1
Robert Zhang <jiaxuanzhang066@gmail.com>
2023-01-01 14:20:03 -0500

    [Refactor][Tests] Optionally run benchmarks with `--bench`
    This PR adds a `--bench` commandline flag to conditionally run the benchmarks
    for the DDE interpreter, saving time if we only want to run unit tests.
    
    This PR also fixes the benchmarks to truly run the interpreter on fibonnaci
    programs of *different* inputs. The result of the benchmarks is consistent
    with the expected linear time complexity of the interpreter when run on
    exponential time algorithms.
    
    Test plan:
    
    `dune exec -- ./tests/tests.exe --bench` to run both the benchmarks and unit
    tests. The running time of interpretation increases linearly against inputs to
    functions and all tests pass.
    
    ghstack-source-id: ff19da78fb291785517f1370f6eebf27c9d16c36
    Pull Request resolved: https://github.com/JHU-PL-Lab/dde/pull/10

--- tests/tests.ml
+++ tests/tests.ml
@@ -4,4 +4,8 @@ let tests =
   "All tests"
   >::: [ Tests_subst.dde_subst; Tests_env.dde_env; Tests_self.dde_self ]
 
-let () = run_test_tt_main tests
+let () =
+  let bench = ref false in
+  Arg.parse [ ("--bench", Arg.Set bench, "run benchmarks") ] (fun _ -> ()) "";
+  if !bench then Benchmarks.fib_bench ();
+  run_test_tt_main tests
--- tests/tests_self.ml
+++ tests/tests_self.ml
@@ -1,6 +1,5 @@
 open OUnit2
 open Utils
-open Core_bench
 
 let test_laziness _ =
   assert_equal
@@ -12,22 +11,6 @@ let test_laziness _ =
 
 let test_memoization _ =
   assert_equal (dde_eval (Tests_subst.dde_fib 100)) (dde_parse "5050")
-
-(* TODO: mutable vs immutable data structures *)
-(* Benchmark fibonacci to see that it's linear time as intermediate interpretation results are cached *)
-let fib_bench =
-  Command_unix.run
-    (Bench.make_command
-       [
-         Bench.Test.create ~name:"fib 25" (fun () ->
-             dde_eval (Tests_subst.dde_fib 100));
-         Bench.Test.create ~name:"fib 50" (fun () ->
-             dde_eval (Tests_subst.dde_fib 100));
-         Bench.Test.create ~name:"fib 75" (fun () ->
-             dde_eval (Tests_subst.dde_fib 100));
-         Bench.Test.create ~name:"fib 100" (fun () ->
-             dde_eval (Tests_subst.dde_fib 100));
-       ])
 
 let dde_self_tests =
   [ "Laziness" >:: test_laziness; "Memoization" >:: test_memoization ]
