71e0ba161e41e402fa3622217e59aaf2bfa9cf76
Robert Zhang <jiaxuanzhang066@gmail.com>
2023-02-03 09:00:11 -0500

    [PA] Use Hashset over list where necessary
    Use a mutable set data structure for the set of call site stacks and
    the set of visited program states, for better readability and performance.
    
    [ghstack-poisoned]

--- program_analysis/debugutils.ml
+++ program_analysis/debugutils.ml
@@ -4,8 +4,8 @@ let analyze = Lib.analyze
 let parse s = s ^ ";;" |> Lexing.from_string |> Ddeparser.main Ddelexer.token
 let unparse v = v |> Format.asprintf "%a" Utils.pp_result_value
 let parse_analyze s = s |> parse |> analyze
-let parse_analyze_unparse s = s |> parse |> analyze |> fst |> unparse
+let parse_analyze_unparse s = s |> parse |> analyze |> unparse
 let pau = parse_analyze_unparse
 
 let parse_eval_print s =
-  s |> parse |> analyze |> fst |> Format.printf "==> %a\n" Utils.pp_result_value
+  s |> parse |> analyze |> Format.printf "==> %a\n" Utils.pp_result_value
--- program_analysis/lib.ml
+++ program_analysis/lib.ml
@@ -4,8 +4,6 @@ exception Unreachable
 
 type label_t = int [@@deriving show { with_path = false }]
 type sigma_t = label_t list [@@deriving show { with_path = false }]
-type set_t = sigma_t list
-type vis_t = (sigma_t * expr) list
 
 type op_result_value =
   | PlusOp of result_value * result_value
@@ -45,43 +43,39 @@ let rec fold_choices f accum choices =
   | res :: rest -> fold_choices f (f res accum) rest
   | [] -> accum
 
-(* TODO: use hashset for S *)
-let rec analyze_aux e sigma set vis =
+let set : sigma_t Hashset.t = Hashset.create 100
+let vis : (expr * sigma_t) Hashset.t = Hashset.create 50
+
+let rec analyze_aux e sigma =
   match e with
-  | Int i -> (IntResult i, set)
-  | Bool b -> (BoolResult b, set)
+  | Int i -> IntResult i
+  | Bool b -> BoolResult b
   | Function (_, _, l) ->
       (*? should ChoiceResult wrap all results? *)
-      ( ChoiceResult { choices = [ FunResult { f = e; l; sigma } ]; l; sigma },
-        set )
+      ChoiceResult { choices = [ FunResult { f = e; l; sigma } ]; l; sigma }
   | Appl (e', _, l) -> (
       (* Stub *)
       let sigma_app_l = prune_sigma (l :: sigma) in
-      match
-        List.find_opt (fun (e', sigma') -> e = e' && sigma_app_l = sigma') vis
-      with
-      | Some _ -> (StubResult { e; sigma = sigma_app_l }, set)
-      | None -> (
-          (* Application *)
-          match analyze_aux e' sigma set vis with
-          | ChoiceResult { choices; _ }, set_1 ->
-              let result_list, set_union =
-                fold_choices
-                  (fun fun_res (result_accum, set_accum) ->
-                    match fun_res with
-                    | FunResult { f = Function (_, e_i, _); _ } ->
-                        let res_i, set_i =
-                          analyze_aux e_i sigma_app_l
-                            ((l :: sigma) :: set_accum)
-                            ((e, sigma_app_l) :: vis)
-                        in
-                        (res_i :: result_accum, set_i)
-                    | _ -> failwith "funresult (appl)" [@coverage off])
-                  ([], set_1) choices
-              in
-              ( ChoiceResult { choices = result_list; l; sigma = sigma_app_l },
-                set_union )
-          | _ -> failwith "choice (appl)" [@coverage off]))
+      if Hashset.mem vis (e, sigma_app_l) then
+        StubResult { e; sigma = sigma_app_l }
+      else
+        (* Application *)
+        match analyze_aux e' sigma with
+        | ChoiceResult { choices; _ } ->
+            let result_list =
+              fold_choices
+                (fun fun_res accum ->
+                  match fun_res with
+                  | FunResult { f = Function (_, e_i, _); _ } ->
+                      Hashset.add set (l :: sigma);
+                      Hashset.add vis (e, sigma_app_l);
+                      let res_i = analyze_aux e_i sigma_app_l in
+                      res_i :: accum
+                  | _ -> failwith "funresult (appl)" [@coverage off])
+                [] choices
+            in
+            ChoiceResult { choices = result_list; l; sigma = sigma_app_l }
+        | _ -> failwith "choice (appl)" [@coverage off])
   | Var (Ident x, l) -> (
       let sigma_hd = List.hd sigma in
       let sigma_tl = List.tl sigma in
@@ -92,31 +86,27 @@ let rec analyze_aux e sigma set vis =
             match get_expr sigma_hd with
             | Appl (_, e2, l') ->
                 (* enumerate all matching stacks in the set *)
-                let result_list, set_union =
-                  List.fold_left
-                    (fun ((result_accum, set_accum) as accum) sigma_i ->
+                let result_list =
+                  Hashset.fold
+                    (fun sigma_i accum ->
                       if
                         List.hd sigma_i = l'
                         && contains_sigma (List.tl sigma_i) sigma_tl
-                      then
-                        let res_i, set_i =
-                          analyze_aux e2 (List.tl sigma_i) set_accum vis
-                        in
-                        (res_i :: result_accum, set_i)
+                      then analyze_aux e2 (List.tl sigma_i) :: accum
                       else accum)
-                    ([], set) set
+                    set []
                 in
-                (ChoiceResult { choices = result_list; l; sigma }, set_union)
+                ChoiceResult { choices = result_list; l; sigma }
             | _ -> failwith "appl (var local)" [@coverage off]
           else
             (* Var Non-Local *)
             match get_expr sigma_hd with
             | Appl (e1, _, l2) -> (
-                match analyze_aux e1 sigma set vis with
-                | ChoiceResult { choices; _ }, set_1 ->
-                    let result_list, set_union =
+                match analyze_aux e1 sigma with
+                | ChoiceResult { choices; _ } ->
+                    let result_list =
                       fold_choices
-                        (fun fun_res (result_accum, set_accum) ->
+                        (fun fun_res accum ->
                           match fun_res with
                           | FunResult
                               {
@@ -124,66 +114,51 @@ let rec analyze_aux e sigma set vis =
                                 l = l1;
                                 sigma = sigma_i;
                               } ->
-                              let res_i, set_i =
-                                analyze_aux
-                                  (Var (Ident x, l1))
-                                  sigma_i set_accum vis
-                              in
-                              (res_i :: result_accum, set_i)
-                          | _ ->
-                              (* TODO: throw or skip *)
-                              failwith "FunResult (var non-local)"
-                              [@coverage off])
-                        ([], set_1) choices
+                              analyze_aux (Var (Ident x, l1)) sigma_i :: accum
+                          | _ -> accum)
+                        [] choices
                     in
-                    (ChoiceResult { choices = result_list; l; sigma }, set_union)
+                    ChoiceResult { choices = result_list; l; sigma }
                 | _ -> failwith "choice" [@coverage off])
             | _ -> failwith "appl" [@coverage off])
       | _ -> failwith "function" [@coverage off])
   | Plus (e1, e2, _) ->
       (* hereafter we use short forms `r1` (`res1`), `s2` (`set2`), etc.
          as code is clearer here and thus they are less ambiguous. *)
-      let vis' = (e, sigma) :: vis in
-      let r1, s1 = analyze_aux e1 sigma set vis' in
-      let r2, s2 = analyze_aux e2 sigma s1 vis' in
-      (OpResult (PlusOp (r1, r2)), s2)
+      let r1 = analyze_aux e1 sigma in
+      let r2 = analyze_aux e2 sigma in
+      OpResult (PlusOp (r1, r2))
   | Minus (e1, e2, _) ->
-      let vis' = (e, sigma) :: vis in
-      let r1, s1 = analyze_aux e1 sigma set vis' in
-      let r2, s2 = analyze_aux e2 sigma s1 vis' in
-      (OpResult (MinusOp (r1, r2)), s2)
+      let r1 = analyze_aux e1 sigma in
+      let r2 = analyze_aux e2 sigma in
+      OpResult (MinusOp (r1, r2))
   | Equal (e1, e2, _) ->
-      let vis' = (e, sigma) :: vis in
-      let r1, s1 = analyze_aux e1 sigma set vis' in
-      let r2, s2 = analyze_aux e2 sigma s1 vis' in
-      (OpResult (EqualOp (r1, r2)), s2)
+      let r1 = analyze_aux e1 sigma in
+      let r2 = analyze_aux e2 sigma in
+      OpResult (EqualOp (r1, r2))
   | And (e1, e2, _) ->
-      let vis' = (e, sigma) :: vis in
-      let r1, s1 = analyze_aux e1 sigma set vis' in
-      let r2, s2 = analyze_aux e2 sigma s1 vis' in
-      (OpResult (AndOp (r1, r2)), s2)
+      let r1 = analyze_aux e1 sigma in
+      let r2 = analyze_aux e2 sigma in
+      OpResult (AndOp (r1, r2))
   | Or (e1, e2, _) ->
-      let vis' = (e, sigma) :: vis in
-      let r1, s1 = analyze_aux e1 sigma set vis' in
-      let r2, s2 = analyze_aux e2 sigma s1 vis' in
-      (OpResult (OrOp (r1, r2)), s2)
+      let r1 = analyze_aux e1 sigma in
+      let r2 = analyze_aux e2 sigma in
+      OpResult (OrOp (r1, r2))
   | Not (e', _) ->
-      let r, s = analyze_aux e' sigma set ((e, sigma) :: vis) in
-      (OpResult (NotOp r), s)
+      let r = analyze_aux e' sigma in
+      OpResult (NotOp r)
   | If (e', e1, e2, l) ->
-      let vis' = (e, sigma) :: vis in
-      let _r, s0 = analyze_aux e' sigma set vis' in
+      let _r = analyze_aux e' sigma in
       (* TODO: eval r *)
       (* on stub, denote as `anynum` *)
-      let r_true, s_true = analyze_aux e1 sigma set vis' in
-      let r_false, s_false = analyze_aux e2 sigma set vis' in
-      ( ChoiceResult { choices = [ r_true; r_false ]; l; sigma },
-        s0 @ s_true @ s_false )
+      let r_true = analyze_aux e1 sigma in
+      let r_false = analyze_aux e2 sigma in
+      ChoiceResult { choices = [ r_true; r_false ]; l; sigma }
   | Let _ -> raise Unreachable [@coverage off]
 
 let analyze e =
   let e = transform_let e in
   fill_my_fun e None;
-  analyze_aux e [] [] []
+  analyze_aux e []
 
 (* TODO: multiple layers of ChoiceResult - improve output readability *)
