54cf3838e4d3039468930d4c30450893fa0bec53
Robert Zhang <jiaxuanzhang066@gmail.com>
2023-02-03 23:26:50 -0500

    Update on "[WIP][Tests] Set up test infra for program analysis"
    [ghstack-poisoned]

--- program_analysis/tests/tests_self.ml
+++ program_analysis/tests/tests_self.ml
@@ -1,18 +1,12 @@
 (* open OUnit2
    open Utils
 
-   let test_laziness _ =
+   let pa_basics _ =
      assert_equal
        (dde_eval_fb "(fun x -> fun y -> x) ((fun z -> z + 1) (1 + 2 + 3));;")
-       (dde_parse "fun y -> 7");
-     assert_equal
-       (dde_eval_fb "(fun x -> fun y -> x) (if true then 1 else 0);;")
-       (dde_parse "fun y -> 1")
+       (dde_parse "fun y -> 7")
 
-   let test_memoization _ =
-     assert_equal (dde_eval_fb (Tests_subst.dde_fib 100)) (dde_parse "5050")
-
-   let dde_self_tests =
+   let pa_self_tests =
      [ "Laziness" >:: test_laziness; "Memoization" >:: test_memoization ]
 
-   let dde_self = "DDE against self" >::: dde_self_tests *)
+   let pa_self = "Program analysis against self" >::: dde_self_tests *)
