091e5c7597c0e8cba6baa2e3dfbe679b81299c97
Robert Zhang <jiaxuanzhang066@gmail.com>
2023-03-12 23:48:20 -0400

    Update base for Update on "[Tests][PA] Set up property-based testing"
    This commit orchestrates property-based testing for program analysis
    using Core.Quickcheck. As a first step, it checks for unclosed programs
    and assigns an reasonable initial weight to different expressions (e.g.,
    Int and Bool are to occur less frequently).
    
    Test plan:
    
    `dune test program_analysis`
    
    [ghstack-poisoned]

--- program_analysis/tests/tests_self.ml
+++ program_analysis/tests/tests_self.ml
@@ -9,12 +9,6 @@ let test_nonlocal _ =
   assert_equal "| (| | 1 + | 2)" (pau "((fun x -> fun y -> x + y) 1) 2;;");
   assert_equal "| | (| | 1 + | 2)" (pau "(fun x -> (fun y -> x + y) 2) 1;;")
 
-let test_stub _ =
-  assert_equal "TODO"
-    (pau
-       "let id = fun self -> fun n -> if n = 0 then 0 else 1 + self self (x - \
-        1) in id id 10;;")
-
 let pa_self_tests =
   [ "Basics" >:: test_basics; "Non-local variable lookup" >:: test_nonlocal ]
 
