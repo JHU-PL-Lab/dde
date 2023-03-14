7505b49b89d37e1e11a56e8da68251c79c0c1ddd
Robert Zhang <jiaxuanzhang066@gmail.com>
2023-03-13 20:19:29 -0400

    [WIP][Tests][PA] Enhance and validate randomized tests
    This commit optimizes the generated ASTs as much as possible to get good
    test cases. When possible, a free variable use is replaced with a use of
    a random preceding definition. Fb's `eval` is then run on each generated
    AST to validate that it's closed and devoid of runtime type errors.
    
    Test case:
    
    `dune test program_analysis`
    
    [ghstack-poisoned]

--- program_analysis/tests/tests_prop.ml
+++ program_analysis/tests/tests_prop.ml
@@ -1,42 +1,99 @@
 open Program_analysis.Lib
 open Test_utils
 open Ddeast
+module QC = Core.Quickcheck
 
-let set = Hashset.create 100
+module IdentSet = Set.Make (struct
+  type t = ident
 
-let rec is_closed e =
+  let compare ident1 ident2 =
+    match (ident1, ident2) with Ident id1, Ident id2 -> compare id1 id2
+end)
+
+let label = ref (-1)
+
+let fresh_label () =
+  label := !label + 1;
+  !label
+
+let reset_label () = label := -1
+
+let rec rename_vars ?(vars = IdentSet.empty) e =
   match e with
-  | Int _ | Bool _ -> true
-  | Var (x, _) -> Hashset.mem set x
-  | Let (id, e1, e2, _) ->
-      is_closed e1
-      &&
-      let _ = Hashset.add set id in
-      is_closed e2
+  | Int _ | Bool _ -> e
   | Function (id, e, _) ->
-      let _ = Hashset.add set id in
-      is_closed e
-  | Appl (e1, e2, _)
-  | Plus (e1, e2, _)
-  | Minus (e1, e2, _)
-  | Equal (e1, e2, _)
-  | And (e1, e2, _)
+      let vars = IdentSet.add id vars in
+      Function (id, rename_vars e ~vars, fresh_label ())
+  | Var (id, _) -> (
+      match IdentSet.find_opt id vars with
+      | Some _ -> Var (id, fresh_label ())
+      | None -> (
+          match IdentSet.choose_opt vars with
+          | Some (Ident chosen_id) -> Var (Ident chosen_id, fresh_label ())
+          | None -> Var (id, fresh_label ())))
+  | Appl (e1, e2, _) ->
+      Appl (rename_vars e1 ~vars, rename_vars e2 ~vars, fresh_label ())
+  | If (e1, e2, e3, _) ->
+      If
+        ( rename_vars e1 ~vars,
+          rename_vars e2 ~vars,
+          rename_vars e3 ~vars,
+          fresh_label () )
+  | Plus (e1, e2, _) ->
+      Plus (rename_vars e1 ~vars, rename_vars e2 ~vars, fresh_label ())
+  | Minus (e1, e2, _) ->
+      Minus (rename_vars e1 ~vars, rename_vars e2 ~vars, fresh_label ())
+  | Equal (e1, e2, _) ->
+      Equal (rename_vars e1 ~vars, rename_vars e2 ~vars, fresh_label ())
+  | And (e1, e2, _) ->
+      And (rename_vars e1 ~vars, rename_vars e2 ~vars, fresh_label ())
   | Or (e1, e2, _) ->
-      is_closed e1 && is_closed e2
-  | Not (e, _) -> is_closed e
-  | If (e1, e2, e3, _) -> is_closed e1 && is_closed e2 && is_closed e3
+      Or (rename_vars e1 ~vars, rename_vars e2 ~vars, fresh_label ())
+  | Not (e, _) -> Not (rename_vars e ~vars, fresh_label ())
+  | Let _ -> failwith "unreachable"
+
+let ( |>% ) v f = Option.map f v
+
+let rec strip_label_fb (e : Ddeast.expr) : Fbast.expr =
+  match e with
+  | Int i -> Int i
+  | Bool b -> Bool b
+  | Function (Ident x, e, _) -> Function (Ident x, strip_label_fb e)
+  | Appl (e1, e2, _) -> Appl (strip_label_fb e1, strip_label_fb e2)
+  | Var (Ident x, _) -> Var (Ident x)
+  | Plus (e1, e2, _) -> Plus (strip_label_fb e1, strip_label_fb e2)
+  | Minus (e1, e2, _) -> Minus (strip_label_fb e1, strip_label_fb e2)
+  | Equal (e1, e2, _) -> Equal (strip_label_fb e1, strip_label_fb e2)
+  | And (e1, e2, _) -> And (strip_label_fb e1, strip_label_fb e2)
+  | Or (e1, e2, _) -> Or (strip_label_fb e1, strip_label_fb e2)
+  | Not (e, _) -> Not (strip_label_fb e)
+  | If (e1, e2, e3, _) ->
+      If (strip_label_fb e1, strip_label_fb e2, strip_label_fb e3)
+  | _ -> raise Unreachable
 
 let run () =
-  Core.Quickcheck.test quickcheck_generator_expr ~sexp_of:sexp_of_expr
-    ~trials:10000
-    ~sizes:
-      (Base.Sequence.cycle_list_exn (Core.List.range 0 20 ~stop:`inclusive))
+  QC.test quickcheck_generator_expr (* ~sexp_of:sexp_of_expr *)
+    ~sexp_of:(fun e -> e |> rename_vars |> sexp_of_expr)
+    ~trials:1000
+    ~sizes:(Base.Sequence.cycle_list_exn (Core.List.range 3 5 ~stop:`inclusive))
+    ~seed:`Nondeterministic
     ~f:(fun e ->
-      if is_closed e then
-        e |> au
-        (* |> Format.asprintf "%a" pp_expr *)
-        |> print_endline;
-      Hashset.clear set (*|> ignore*))
+      e |> rename_vars
+      |> (fun e ->
+           reset_label ();
+           e)
+      |> (fun e ->
+           try
+             (* run Fb interpreter to validate (closed + well-typed) *)
+             e |> strip_label_fb |> Fbinterp.eval |> ignore;
+             Some e
+           with _ -> None)
+      |>% au
+      (* |>% (fun e -> Format.asprintf "%a" pp_expr e) *)
+      |>% (fun s ->
+            print_endline s;
+            print_endline "")
+      |> ignore)
 
 (*
   TODOs:
