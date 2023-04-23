f6a971153a6573da281ff07b9a968ee6eb03c9c1
Robert Zhang <jiaxuanzhang066@gmail.com>
2023-04-23 13:12:16 -0400

    [Formal] Simplify notations and fix notation scoping
    Takes a more disciplined approach to defining notations for the core DDE
    language, reflected through better precedence levels and correspondence
    between notations and grammar. The result is more concise and more
    versatile notations.
    
    This commit also switches to using function types for constructors of
    inductive type definitions over using constructor arguments because the
    former is arguably more concise.
    
    Test plan:
    
    `make` to see that everything typechecks.
    
    [ghstack-poisoned]

--- formal/Core.v
+++ formal/Core.v
@@ -6,15 +6,15 @@ Import ListNotations.
 
 (* slightly adjusted grammar to more easily define notations *)
 Inductive expr : Type :=
-  | Appl (e1 : lexpr) (e2 : lexpr)
-  | Val (v : val)
-  | Ident (id : string)
-with lexpr : Type := Lexpr (e : expr) (l : nat)
-with val : Type := Fun (id : string) (e : lexpr).
+  | Appl: lexpr -> lexpr -> expr
+  | Val: val -> expr
+  | Ident: string -> expr
+with lexpr : Type := Lexpr : expr -> nat -> lexpr
+with val : Type := Fun : string -> lexpr -> val.
 
 Definition sigma : Type := list nat.
 
-Inductive res : Type := Res (v : val) (l : nat) (s : sigma).
+Inductive res : Type := Res : val -> nat -> sigma -> res.
 
 (* coerce strings into exprs via the Ident constructor *)
 Coercion Ident : string >-> expr.
@@ -24,26 +24,24 @@ Declare Scope lang_scope.
 
 (* custom language syntax *)
 Notation "<{ e }>" := e (at level 0, e custom lang at level 99) : lang_scope.
-Notation "( x )" := x (in custom lang, x at level 99) : lang_scope.
+Notation "( x )" := x (in custom lang at level 0, x at level 99) : lang_scope.
 Notation "x" := x (in custom lang at level 0, x constr at level 0) : lang_scope.
-(* TODO: next level? *)
 Notation "'fun' x -> e" :=
   (Fun x e)
-    (in custom lang at level 20, e at level 30, right associativity) : lang_scope.
+    (in custom lang at level 20, e at next level, right associativity) : lang_scope.
 Notation "e1 <- e2" :=
   (Appl e1 e2)
     (* associativity doesn't matter here as e1 and e2 are lexprs *)
     (in custom lang at level 30, left associativity).
+Notation "$ v" :=
+  (Val v)
+    (in custom lang at level 25) : lang_scope.
 Notation "e @ l" :=
   (Lexpr e l)
-    (in custom lang at level 15, no associativity) : lang_scope.
-Notation "v @@ l" :=
-  (Lexpr (Val v) l)
-    (in custom lang at level 15, no associativity) : lang_scope.
-(* TODO: meh syntax to make parser happy *)
-Notation "#[ v , l , s ]" :=
+    (in custom lang at level 15) : lang_scope.
+Notation "[ v , l , s ]" :=
   (Res v l s)
-    (in custom lang at level 15, v at level 25, l at level 25, s at level 25, no associativity) : lang_scope.
+    (in custom lang at level 99, v at next level, l at next level, s constr) : lang_scope.
 
 Open Scope lang_scope.
 
@@ -55,10 +53,10 @@ Definition N : string := "n".
 
 Example sample_ident := <{ X@0 }>.
 Example sample_fun := <{ fun X -> X@0 }>.
-Example sample_fun_lexpr := <{ (fun X -> X@0) @@ 1 }>.
-Example sample_fun_lexpr' := <{ (fun X -> (fun Y -> X@0) @@ 1) @@ 2 }>.
+Example sample_fun_lexpr := <{ ($fun X -> X@0) @ 1 }>.
+Example sample_fun_lexpr' := <{ ($fun X -> ($fun Y -> X@0) @ 1) @ 2 }>.
 Example sample_appl := <{ (X@0 <- X@1) @ 2}>.
-Example sample_res := <{ #[ fun X -> X@0, 1, [] ] }>.
+Example sample_res := <{ [ fun X -> X@0, 1, 0 :: [] ] }>.
 
 (* TODO: auto-generate labels *)
 
@@ -99,43 +97,43 @@ Fixpoint build_mylexpr (le : lexpr) (ml : mylexpr) : mylexpr :=
 
 (* custom syntax for evaluation; I try to mirror the written syntax as much as possible *)
 Reserved Notation
-         "{{ mf , ml , s }} |- e => r"
-         (at level 40, e custom lang at level 99,
-          mf constr, ml constr, s constr, r custom lang at level 99).
+         "mf / ml / s |- e => r"
+         (at level 40, e custom lang at level 99, r custom lang at level 99,
+          ml at next level, s at next level).
 
 (* logic for DDE's concrete lambda calculus operational semantics *)
 Inductive eval : lexpr -> sigma -> res -> myfun -> mylexpr -> Prop :=
   | E_Val : forall s v l mf ml,
-    {{ mf, ml, s }} |- v@@l => #[ v, l, s ]
+    mf / ml / s |- ($v)@l => [ v, l, s ]
   (* TODO: forall sound? *)
   | E_Appl : forall e1 e2 l s r x e l1 s1 mf ml,
-    {{ mf, ml, s }} |- e1 => #[ fun x -> e, l1, s1 ] ->
-    {{ mf, ml, l :: s }} |- e => r ->
-    {{ mf, ml, s }} |- (e1 <- e2) @ l => r
+    mf / ml / s |- e1 => [ fun x -> e, l1, s1 ] ->
+    mf / ml / l :: s |- e => r ->
+    mf / ml / s |- (e1 <- e2) @ l => r
   | E_VarLocal : forall (x : string) l s r mf ml e1 e2 l' e mf_l,
     List.length s <> 0 ->
     (* hd will never give default *)
     ml (hd 0 s) = Some <{ (e1 <- e2) @ l' }> ->
     mf l = Some mf_l ->
-    ml mf_l = Some <{ (fun x -> e) @@ mf_l }> ->
-    {{ mf, ml, tl s }} |- e2 => r ->
-    {{ mf, ml, s }} |- x@l => r
+    ml mf_l = Some <{ ($fun x -> e) @ mf_l }> ->
+    mf / ml / tl s |- e2 => r ->
+    mf / ml / s |- x@l => r
   | E_VarNonLocal : forall (x : string) l s r mf ml e1 e2 l2 e mf_l x1 s1,
     List.length s <> 0 ->
     ml (hd 0 s) = Some <{ (e1 <- e2) @ l2 }> ->
     mf l = Some mf_l ->
-    ml mf_l = Some <{ (fun x1 -> e) @@ mf_l }> ->
+    ml mf_l = Some <{ ($fun x1 -> e) @ mf_l }> ->
     x <> x1 ->
-    {{ mf, ml, tl s }} |- e1 => #[ fun x1 -> e, mf_l, s1 ] ->
-    {{ mf, ml, s1 }} |- x@mf_l => r ->
-    {{ mf, ml, s }} |- x@l => r
+    mf / ml / tl s |- e1 => [ fun x1 -> e, mf_l, s1 ] ->
+    mf / ml / s1 |- x@mf_l => r ->
+    mf / ml / s |- x@l => r
 
-  where "{{ mf , ml , s }} |- e => r" := (eval e s r mf ml).
+  where "mf / ml / s |- e => r" := (eval e s r mf ml).
 
 Hint Unfold build_myfun build_mylexpr update t_update : core.
 
 Definition id_val :=
-  <{ (fun X -> X@0) @@ 1 }>.
+  <{ ($fun X -> X@0) @ 1 }>.
 Definition id_val_mf :=
   build_myfun id_val None empty.
 Definition id_val_ml :=
@@ -143,13 +141,13 @@ Definition id_val_ml :=
 
 (* simple value *)
 Example eg_val_correct :
-  {{ id_val_mf, id_val_ml, [] }} |- id_val => #[ fun X -> X@0, 1, [] ].
+  id_val_mf / id_val_ml / [] |- id_val => [ fun X -> X@0, 1, [] ].
 Proof.
   apply E_Val.
 Qed.
 
 Definition eg_loc :=
-  <{ ((fun X -> X@0) @@ 1 <- (fun Y -> Y@2) @@ 3) @ 4 }>.
+  <{ (($fun X -> X@0) @ 1 <- ($fun Y -> Y@2) @ 3) @ 4 }>.
 Definition eg_loc_mf :=
   build_myfun eg_loc None empty.
 Definition eg_loc_ml :=
@@ -160,7 +158,7 @@ Definition eg_loc_ml :=
    so that the proof can be better traced to see what's happening. Proof
    scripts can be almost entirely automated in our logic. *)
 Example eg_loc_correct :
-  {{ eg_loc_mf, eg_loc_ml, [] }} |- eg_loc => #[ fun Y -> Y@2, 3, [] ].
+  eg_loc_mf / eg_loc_ml / [] |- eg_loc => [ fun Y -> Y@2, 3, [] ].
 Proof.
   eapply E_Appl.
   - apply E_Val.
@@ -173,9 +171,9 @@ Proof.
 Qed.
 
 Definition eg_noloc :=
-  <{ (((fun X -> (fun Y -> X@0) @@ 1) @@ 2
-      <- (fun Z -> Z@3) @@ 4) @ 5
-      <- (fun M -> M@6) @@ 7) @ 8 }>.
+  <{ ((($fun X -> ($fun Y -> X@0) @ 1) @ 2
+      <- ($fun Z -> Z@3) @ 4) @ 5
+      <- ($fun M -> M@6) @ 7) @ 8 }>.
 Definition eg_noloc_mf :=
   build_myfun eg_noloc None empty.
 Definition eg_noloc_ml :=
@@ -183,7 +181,7 @@ Definition eg_noloc_ml :=
 
 (* non-local variable lookup *)
 Example eg_noloc_correct :
-  {{ eg_noloc_mf, eg_noloc_ml, [] }} |- eg_noloc => #[ fun Z -> Z@3, 4, [] ].
+  eg_noloc_mf / eg_noloc_ml / [] |- eg_noloc => [ fun Z -> Z@3, 4, [] ].
 Proof.
   eapply E_Appl.
   - eapply E_Appl.
@@ -216,7 +214,7 @@ Ltac map_lookup mymap prog :=
 
 (* bad non-local variable lookup *)
 Example eg_noloc_bad :
-  ~ {{ eg_noloc_mf, eg_noloc_ml, [] }} |- eg_noloc => #[ fun M -> M@6, 7, [] ].
+  ~ eg_noloc_mf / eg_noloc_ml / [] |- eg_noloc => [ fun M -> M@6, 7, [] ].
 Proof.
   intro contra.
   inversion contra. subst. clear contra.
@@ -256,8 +254,8 @@ Ltac injection_subst :=
   end.
 
 Theorem eval_deterministic : forall mf ml s e r1 r2,
-  {{ mf, ml, s }} |- e => r1 ->
-  {{ mf, ml, s }} |- e => r2 ->
+  mf / ml / s |- e => r1 ->
+  mf / ml / s |- e => r2 ->
   r1 = r2.
 Proof.
   intros. generalize dependent r2. induction H; intros.
@@ -285,3 +283,6 @@ Proof.
       apply IHeval1 in H14. injection H14 as H14. subst.
       apply IHeval2 in H19. assumption.
 Qed.
+
+Close Scope lang_scope.
+
