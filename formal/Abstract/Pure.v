804b28f49d703d58be27c1e6af9954bddcff8a00
Robert Zhang <jiaxuanzhang066@gmail.com>
2023-05-16 20:31:45 -0400

    [Formal][Refactor] Extract and simplify common utilities
    This commit extracts common utilities in Abstract into a separate
    module and simplifies calls to `build_myfun` and `build_mylexpr` to just
    have one argument - the root labeled expression.
    
    Test plan:
    
    `dune build formal` and see that everything compiles.
    
    ghstack-source-id: bc2e986ed9336f606003bba77d0f5e300cee9261
    Pull Request resolved: https://github.com/JHU-PL-Lab/dde/pull/63

--- formal/Abstract/Pure.v
+++ formal/Abstract/Pure.v
@@ -1,19 +1,6 @@
-From Coq Require Import Arith.Arith.
-From DDE.Common Require Import Lang.
-
-Definition s_set : Type := list sigma.
+From DDE.Abstract Require Import Common.
 
 #[local] Open Scope lang_scope.
-
-Fixpoint prune_sigma (s : sigma) (k : nat) (acc : sigma) : sigma := 
-  match s with
-  | [] => acc
-  | l :: ls =>
-    if k =? 0 then acc
-    else prune_sigma ls (k - 1) (l :: acc)
-  end.
-
-Definition prune_sigma_2 (s : sigma) : sigma := prune_sigma s 2 [].
 
 Reserved Notation
          "mf / ml / s / S '|-a' e => rv / Sv"
@@ -23,21 +10,21 @@ Reserved Notation
 Inductive analyze
   : myfun -> mylexpr -> sigma -> s_set -> lexpr -> res -> s_set -> Prop
 :=
-  | E_Val : forall mf ml s S v l,
+  | A_Val : forall mf ml s S v l,
     mf / ml / s / S |-a ($v)@l => [ v, l, s ] / S
-  | E_Appl : forall mf ml s S e1 e2 l rv Sv x e l1 s1 S1,
+  | A_Appl : forall mf ml s S e1 e2 l rv Sv x e l1 s1 S1,
     mf / ml / s / S |-a e1 => [ fun x *-> e, l1, s1 ] / S1 ->
-    mf / ml / prune_sigma_2 (l :: s) / ((l :: s) :: S1) |-a e => rv / Sv ->
+    mf / ml / prune_sigma2 (l :: s) / ((l :: s) :: S1) |-a e => rv / Sv ->
     mf / ml / s / S |-a (e1 <-* e2) @ l => rv / Sv
   (* TODO: should s' be universally/existentially quantified? *)
-  | E_VarLocal : forall mf ml e1 e2 l' s S x l rv Sv mf_l e s',
+  | A_VarLocal : forall mf ml e1 e2 l' s S x l rv Sv mf_l e s',
     mf l = Some mf_l ->
     ml mf_l = Some <{ ($fun x *-> e) @ mf_l }> ->
     ml l' = Some <{ (e1 <-* e2) @ l' }> ->
     (exists s0, In s0 S /\ s0 = l' :: s ++ s') ->
     mf / ml / (s ++ s') / S |-a e2 => rv / Sv ->
     mf / ml / (l' :: s) / S |-a x@l => rv / Sv
-  | E_VarNonLocal : forall mf ml e1 e2 l2 s S x l rv Sv x1 e l1 s1 S1,
+  | A_VarNonLocal : forall mf ml e1 e2 l2 s S x l rv Sv x1 e l1 s1 S1,
     mf l = Some l1 ->
     ml l1 = Some <{ ($fun x1 *-> e) @ l1 }> ->
     x <> x1 ->
@@ -54,8 +41,8 @@ Definition self_fun := <{ $fun Self -> $fun N -> Self <- Self }>.
 Definition eg_rec :=
   to_lexpr <{ self_fun <- self_fun <- $fun X -> X }>.
 (* Compute eg_rec. *)
-Definition eg_rec_mf := build_myfun eg_rec None empty.
-Definition eg_rec_ml := build_mylexpr eg_rec empty.
+Definition eg_rec_mf := build_myfun eg_rec.
+Definition eg_rec_ml := build_mylexpr eg_rec.
 
 (* TODO: find examples *)
 Theorem analyze_nondeterministic :
